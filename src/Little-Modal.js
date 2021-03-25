e394290632f3bda6282fe2db87683fe5eafeffdd
LioneNET <pi_kt_s@mail.ru>
2021-03-25 18:58:00 +0300

    date

--- src/Little-Modal.js
+++ src/Little-Modal.js
@@ -55,10 +55,15 @@ function getTemplate(o){
 export default class LittleModal {
 	constructor(element='body', o={}){
 		let $element = getTemplate(this.options)
-
+		this.scope = o.scope || false
 		this.tochX = 0
 		this.tochY = 0
-
+		this.valueX = 0
+		this.valueY = 0
+		this.borderXMin = 0
+		this.borderXMax = 0
+		this.borderYMin = 0
+		this.borderYMax = 0
 		this.maxWidth = o.maxWidth || 500
 		this.maxHeight = o.maxHeight || 300
 		this.options = o
@@ -72,19 +77,27 @@ export default class LittleModal {
 		document.querySelector(element) ? 
 		document.querySelector(element).appendChild($element) : 
 		document.body.insertAdjacentElement('afterbegin', $element);
-		this.elementWidth = this.$element.offsetWidth;
-		this.elementHeight = this.$element.offsetHeight;
-		this.windowWidth = window.innerWidth;
-		this.windowHeight = window.innerHeight;
-		this.limitWidth = this.windowWidth - this.elementWidth;
-		this.limitHeight = this.windowHeight - this.elementHeight;
-		this.valueX = 0;
-		this.valueY = 0;
 
 		this.init();
 	}
 
 	init() {
+		this.elementWidth = this.$element.offsetWidth;
+		this.elementHeight = this.$element.offsetHeight;
+		this.windowWidth = window.innerWidth;
+		this.windowHeight = window.innerHeight;
+
+		//если есть внешние рамки то берем их
+		if(this.scope) {
+			let $scope = document.querySelector(this.scope).getBoundingClientRect()
+			this.windowWidth = $scope.width
+			this.windowHeight = $scope.height
+			
+		}
+
+		this.limitWidth = this.windowWidth - this.elementWidth;
+		this.limitHeight = this.windowHeight - this.elementHeight;
+
 		['mouseDownHandler', 'mouseUpHandler', 'mouseMoveHandler'].forEach( element => this[element] = this[element].bind(this))
 		this.$title.addEventListener('mousedown', this.mouseDownHandler);
 	}
